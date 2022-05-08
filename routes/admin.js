9c24feaccc02bbbd3585f80a5b760b8af247dd43
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-05-08 02:31:27 -0300

    primeira versão do meu blog pronta

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const categoria = require('../models/Categorias');
 
 const router = express.Router();
 //const mongoose = require('mongoose')
@@ -73,9 +74,74 @@ router.post('/categorias/nova',(req,res) => {
     
 })
 
+router.get('/categorias/edit/:id',(req,res) => {
+    Categoria.findOne({_id:req.params.id}).lean().then((categoria) => {
+        res.render('admin/editCategorias', {categoria: categoria})
+    }).catch((err) => {
+        req.flash('error_msg',"Esta categoria não existe")
+        res.redirect("/admin/categorias")
+    })
+    
+})
+
+router.post('/categorias/edit',(req,res) => {
 
 
 
+    var erros = [];
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({
+            texto: "Nome inválido"
+        })
+        
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({
+            texto: "Slug inválido"
+        })
+        
+    }
+
+    if(erros.length > 0){
+        res.redirect("/admin/categorias")
+        
+        
+    } else {
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+        
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+
+        categoria.save().then(() => {
+            res.flash("success_msg","Categoria editada com sucesso")
+            res.redirect("/admin/categorias")
+            
+        }).catch((err) => {
+            req.flash('error_msg',"Erro interno")
+            res.redirect("/admin/categorias")
+        
+        })
+        
+    }).catch((err) => {
+        req.flash('error_msg',"Erro ao editar categoria")
+        res.redirect("/admin/categorias")
+    
+    })
+}
+
+})
+
+router.post('/categorias/deletar',(req,res) => {
+    Categoria.deleteOne({_id:req.body.id}).then(() => {
+        req.flash("success_msg", "categoria deletada com succeso")
+        res.redirect("/admin/categorias")
+    }).catch((err) => {
+        req.flash('error_msg',"erro ao deletar categoriqa")
+        res.redirect("/admin/categorias")
+    })
+})
 
 
 
